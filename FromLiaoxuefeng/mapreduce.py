12b68d8c6292861de4027b715d37a9b01edcefb4
feiwenli <lfw_dar@163.com>
2019-02-01 18:05:52 +0800

    2-1

--- FromLiaoxuefeng/mapreduce.py
+++ FromLiaoxuefeng/mapreduce.py
@@ -1,7 +1,7 @@
-# coding = utf-8
+# -*- coding=utf-8 -*-
 from functools import reduce
 print(list(map(str,[1,2,3,4,5,6,7,8,9])))
-#将str转化为int
+# 将str转化为int
 def str2int(s):
 	def fn(x,y):
 		return x*10+y
@@ -16,4 +16,5 @@ def str2int2(s):
 #利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']：
 def name(n):
 	return n.title()
-print(name("jdfai.flgFJG"))
+print(map(name, ['adam', 'LISA', 'barT']))
+# Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
